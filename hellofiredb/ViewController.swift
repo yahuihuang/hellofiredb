bb94f74e2523befe0f086074edf58a781be16924
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-21 11:28:36 +0800

    write data to firebase realtime database

--- hellofiredb/ViewController.swift
+++ hellofiredb/ViewController.swift
@@ -12,6 +12,8 @@ import Firebase
 class ViewController: UIViewController {
     var dbRef:DatabaseReference!
     
+    @IBOutlet weak var pageTitle: UILabel!
+    @IBOutlet weak var input: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,10 +28,16 @@ class ViewController: UIViewController {
                 let appNameRef = self.dbRef.child("appdefault/name")
                 appNameRef.observeSingleEvent(of: DataEventType.value) { (snapshot) in
                     print("appname: \(snapshot.value as! String)")
+                    self.pageTitle.text = snapshot.value as? String
                 }
                 let appVersionRef = self.dbRef.child("appdefault/version")
                 appVersionRef.observeSingleEvent(of: DataEventType.value) { (snapshot) in
                     print("version: \(snapshot.value as! Double)")
+                }
+                
+                let testRef = self.dbRef.child("test")
+                testRef.observeSingleEvent(of: DataEventType.value) { (snapshot) in
+                    self.input.text = snapshot.value as! String
                 }
             }
         }
@@ -38,6 +46,13 @@ class ViewController: UIViewController {
 
     }
 
-
+    @IBAction func enterAction(_ sender: Any) {
+        let inputValue = input.text ?? ""
+        //取得db reference
+        dbRef = Database.database().reference()
+        
+        dbRef.child("test").setValue(inputValue)
+    }
+    
 }
 

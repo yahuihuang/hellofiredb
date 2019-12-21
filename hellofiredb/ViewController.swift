28f3471d8983a8154f6753081f533fd55ef9ac0e
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-21 10:49:34 +0800

    read data from firebase realtime database

--- hellofiredb/ViewController.swift
+++ hellofiredb/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    var dbRef:DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //取得db reference
+        dbRef = Database.database().reference()
+        let appNameRef = dbRef.child("appdefault/name")
+        appNameRef.observeSingleEvent(of: DataEventType.value) { (snapshot) in
+            print("appname: \(snapshot.value as! String)")
+        }
+        let appVersionRef = dbRef.child("appdefault/version")
+        appVersionRef.observeSingleEvent(of: DataEventType.value) { (snapshot) in
+            print("version: \(snapshot.value as! Double)")
+        }
     }
 
 
